239f2b9a80ca13def77993465a7444b0235f45c7
inceemir <inceemir@spekulatius.wiwi.hu-berlin.de>
2025-04-13 14:58:02 +0200

    timexer trial

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Power Market Modeling
 Causal Analysis of Renewable Energy Penetration and Electricity Price Forecasting with Transformers
 
-### CATE Estimates
-
-<div align="center">
-  <img src="png/mean_vs_cate_solar.png" alt="Solar CATE" width="45%"/>
-  <img src="png/mean_vs_cate_wind.png" alt="Wind CATE" width="45%"/>
-</div>
-
 ### LWPR Results
 
 <div align="center">
   <img src="png/LWPR_solar.png" alt="Solar LWPR" width="45%"/>
   <img src="png/LWPR_wind.png" alt="Wind LWPR" width="45%"/>
+</div>
+
+### CATE Estimates
+
+<div align="center">
+  <img src="png/mean_vs_cate_solar.png" alt="Solar CATE" width="45%"/>
+  <img src="png/mean_vs_cate_wind.png" alt="Wind CATE" width="45%"/>
 </div>
--- scripts/iTransformer.sh
+++ scripts/iTransformer.sh
@@ -1,6 +1,32 @@
 export CUDA_VISIBLE_DEVICES=0
 
-# model_name=iTransformer
+model_name=iTransformer
+
+python -u run.py \
+  --is_training 1 \
+  --task_name long_term_forecast \
+  --root_path ./data/ \
+  --data_path forecast_data.csv \
+  --model_id ECL_168_24 \
+  --model $model_name \
+  --data custom \
+  --features MS \
+  --seq_len 168 \
+  --pred_len 24 \
+  --e_layers 3 \
+  --enc_in 3 \
+  --dec_in 3 \
+  --c_out 1 \
+  --des 'Exp' \
+  --d_model 512 \
+  --d_ff 512 \
+  --batch_size 4 \
+  --learning_rate 0.0005 \
+  --itr 1
+
+# export CUDA_VISIBLE_DEVICES=0
+
+# model_name=TimeXer
 # des='Timexer-MS'
 # patch_len=24
 
@@ -27,28 +53,28 @@ export CUDA_VISIBLE_DEVICES=0
 #   --batch_size 4 \
 #   --itr 1
 
-#   export CUDA_VISIBLE_DEVICES=0
+# export CUDA_VISIBLE_DEVICES=0
 
-model_name=iTransformer
+# model_name=iTransformer_OLD
 
-python -u run.py \
-  --is_training 1 \
-  --task_name long_term_forecast \
-  --root_path ./data/ \
-  --data_path forecast_data.csv \
-  --model_id ECL_96_96 \
-  --model $model_name \
-  --data custom \
-  --features M \
-  --seq_len 96 \
-  --pred_len 96 \
-  --e_layers 3 \
-  --enc_in 321 \
-  --dec_in 321 \
-  --c_out 321 \
-  --des 'Exp' \
-  --d_model 512 \
-  --d_ff 512 \
-  --batch_size 16 \
-  --learning_rate 0.0005 \
-  --itr 1
+# python -u run.py \
+#   --is_training 1 \
+#   --task_name long_term_forecast \
+#   --root_path ./data/ \
+#   --data_path forecast_data.csv \
+#   --model_id ECL_96_96 \
+#   --model $model_name \
+#   --data custom \
+#   --features M \
+#   --seq_len 96 \
+#   --pred_len 96 \
+#   --e_layers 3 \
+#   --enc_in 321 \
+#   --dec_in 321 \
+#   --c_out 321 \
+#   --des 'Exp' \
+#   --d_model 512 \
+#   --d_ff 512 \
+#   --batch_size 16 \
+#   --learning_rate 0.0005 \
+#   --itr 1
